1eb46952823b3ab254f1c01a5b3a1f6679d4fa9c
Arismendi17 <Arismendimiguel6002@protonmail.com>
2026-05-27 10:03:28 -0400

    aaaa

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,18 @@
 function calcular() {
+  let tipoInteres = document.querySelector('input[name="tipoInteres"]:checked').value;
   let P = document.querySelector("#cantPresente").value;
+  let F = document.querySelector("#cantFutura").value;
+  let i = document.querySelector("#tasaInteres").value;
+  let N = document.querySelector("#numeroPeriodos").value;
+  let resultado = document.querySelector("#resultado");
+
+  let output = P * (1 + (i * N));
+  F = output;
+  resultado.textContent = output;
+  console.log(output);
   console.log(P);
+  console.log(N);
+  console.log(F);
+  console.log(i);
+  console.log(tipoInteres);
 }
