44ff38e8d703d40f7f81f4c8ae5e3438ad861eb7
Miguel-Ar06 <marismendi.8551@unimar.edu.ve>
2026-05-27 11:21:38 -0400

    extingan javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
 function calcular() {
-  let tipoInteres = document.querySelector('input[name="tipoInteres"]:checked').value;
-  let P = document.querySelector("#cantPresente").value;
-  let F = document.querySelector("#cantFutura").value;
-  let i = document.querySelector("#tasaInteres").value;
-  let N = document.querySelector("#numeroPeriodos").value;
-  let resultado = document.querySelector("#resultado");
-
-  let output = P * (1 + (i * N));
-  F = output;
+  // elementos
+  const tipoInteres = document.querySelector('input[name="tipoInteres"]:checked');
+  const inputP = document.querySelector("#cantPresente");
+  const inputF = document.querySelector("#cantFutura");
+  const inputi = document.querySelector("#tasaInteres");
+  const inputN = document.querySelector("#numeroPeriodos");
+  const resultado = document.querySelector("#resultado");
+  
+  // inputs
+  let P = parseFloat(inputP.value); 
+  let F = parseFloat(inputF.value);
+  let i = parseFloat(inputi.value);
+  let N = parseFloat(inputN.value);
+  
+  // formulas
+  let output = 0;
+  if (tipoInteres.value === "simple") {
+    output = P * (1 + (i * N));
+  } else {
+    output = P * ((1 + i) ** N);
+  }
+  
+  // resultados
+  output = Math.trunc(output * 10000) / 10000;
+  inputF.value = output;
   resultado.textContent = output;
+  resultado.style.color = "green";
+  
+  // debug
   console.log(output);
   console.log(P);
   console.log(N);
